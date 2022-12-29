7ce5d2c07afbe2449c9941f94d0b7cac9bc1fd53
vveeps <54472340+vveeps@users.noreply.github.com>
2022-12-29 09:03:55 +0200

    move layout to App.tsx

---
+++ pixels/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+// import PixelCanvas from './PixelCanvas';
+
+const App: React.FC = () => (
+  <main>
+    {/*
+    <PixelCanvas
+      position={{ x: 50, y: 50 }} // absolute (x, y) position for canvas
+      canvas_dimensions={{ width: 1600, height: 900 }} // canvas dimensions in px
+      size={13} // general size multiplier
+      grid_width={1} // grid line width
+      grid_dimensions={{ width: 96, height: 64 }} // dimensions of the pixel grid itself
+    />
+  */}
+  </main>
+);
+
+export default App;
--- pixels/src/main.tsx
+++ pixels/src/main.tsx
@@ -1,13 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import PixelCanvas from './PixelCanvas';
+import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <PixelCanvas
-    position={{ x: 50, y: 50 }} // absolute (x, y) position for canvas
-    canvas_dimensions={{ width: 1600, height: 900 }} // canvas dimensions in px
-    size={13} // general size multiplier
-    grid_width={1} // grid line width
-    grid_dimensions={{ width: 96, height: 64 }} // dimensions of the pixel grid itself
-  />,
-);
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />);
